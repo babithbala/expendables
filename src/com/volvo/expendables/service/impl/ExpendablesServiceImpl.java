5bf4d365091a60e9555c6f06ea79c1bea3448483
Tugrul ASLAN <tugrul.aslan@volvo.com>
2017-11-24 20:37:56 +0100

    removed the hardcoded app context loading

--- src/com/volvo/expendables/service/impl/ExpendablesServiceImpl.java
+++ src/com/volvo/expendables/service/impl/ExpendablesServiceImpl.java
@@ -1,14 +1,14 @@
 package com.volvo.expendables.service.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.volvo.expendables.dto.Acknowledge;
 import com.volvo.expendables.dto.ContentDTO;
+import com.volvo.expendables.dto.EventDTO;
+import com.volvo.expendables.dto.PrincipalDTO;
+import com.volvo.expendables.service.impl.mapper.PrincipalRowMapper;
+import com.volvo.expendables.util.ExpendablesUtil;
+import com.volvo.expendables.util.SQL;
 import org.apache.log4j.Logger;
-
-import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,35 +17,42 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.volvo.expendables.dto.Acknowledge;
-import com.volvo.expendables.dto.EventDTO;
-import com.volvo.expendables.dto.PrincipalDTO;
-import com.volvo.expendables.service.impl.mapper.PrincipalRowMapper;
-import com.volvo.expendables.util.ExpendablesUtil;
-import com.volvo.expendables.util.SQL;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Service("expendablesService")
-@Scope("session")
-public class ExpendablesServiceImpl implements ExpendablesService {
+@Component
+public class ExpendablesServiceImpl implements com.volvo.expendables.service.impl.ExpendablesService {
 
-    private static final Logger LOG=Logger.getLogger(ExpendablesServiceImpl.class.getName());
+    private static final Logger LOG = Logger.getLogger(ExpendablesServiceImpl.class.getName());
 
-    private NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(){
-        return new NamedParameterJdbcTemplate(ExpendablesUtil.getDataSource());
+    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    public void setJdbcTemplate(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    @Autowired
+    public void setNamedParameterJdbcTemplate(DataSource dataSource) {
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
 
     /* (non-Javadoc)
      * @see edu.pwr.hc.service.UserInfoService#getPrincipalDetails(java.lang.String)
      */
     public PrincipalDTO getPrincipalDetails(String userName) {
-        JdbcTemplate template = new JdbcTemplate(ExpendablesUtil.getDataSource());
         PrincipalDTO principalInfo = new PrincipalDTO();
         try {
-            principalInfo =  (PrincipalDTO) template.queryForObject(SQL.GET_PRINCIPAL_DETAILS,new Object[] {userName},new PrincipalRowMapper());
+            principalInfo = (PrincipalDTO) jdbcTemplate.queryForObject(SQL.GET_PRINCIPAL_DETAILS, new Object[]{userName}, new PrincipalRowMapper());
         } catch (EmptyResultDataAccessException e) {
             LOG.info(e.getMessage());
         }
@@ -56,70 +63,67 @@ public class ExpendablesServiceImpl implements ExpendablesService {
     public List<Acknowledge> saveEventDetails(EventDTO eventDetails) {
         List<Acknowledge> list = new ArrayList<Acknowledge>();
         Acknowledge acknowledge = new Acknowledge();
-        try{
-            NamedParameterJdbcTemplate template = getNamedParameterJdbcTemplate();
+        try {
             SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(eventDetails);
             GeneratedKeyHolder key = new GeneratedKeyHolder();
 
-            if(ExpendablesUtil.checkNotNull(eventDetails.getStreetName())){
-                template.update(SQL.SAVE_EVENT_ADDRESS, namedParameters,key);
+            if (ExpendablesUtil.checkNotNull(eventDetails.getStreetName())) {
+                jdbcTemplate.update(SQL.SAVE_EVENT_ADDRESS, namedParameters, key);
                 eventDetails.setAddressId(key.getKey().longValue());
                 acknowledge.setIdNext(eventDetails.getAddressId().longValue());
             }
 
-            template.update(SQL.SAVE_EVENT, namedParameters,key);
+            jdbcTemplate.update(SQL.SAVE_EVENT, namedParameters, key);
             eventDetails.setEventId(key.getKey().longValue());
 
             acknowledge.setMessage("User Event details saved. <br/>");
             acknowledge.setId(eventDetails.getEventId().longValue());
 
             list.add(acknowledge);
-            }catch(DataAccessException e){
-                String cause = e.getCause().toString();
-                if(cause !=null){
-                    acknowledge.setMessage(cause.substring(cause.indexOf(":")+1, cause.length())+"<br/>");
-                    list.add(acknowledge);
-                }
-                ExpendablesServiceImpl.LOG.info(e.getMessage());
+        } catch (DataAccessException e) {
+            String cause = e.getCause().toString();
+            if (cause != null) {
+                acknowledge.setMessage(cause.substring(cause.indexOf(":") + 1, cause.length()) + "<br/>");
+                list.add(acknowledge);
             }
-            return list;
+            ExpendablesServiceImpl.LOG.info(e.getMessage());
+        }
+        return list;
     }
 
     @Transactional
     public List<Acknowledge> updateEventDetails(EventDTO eventDetails) {
         List<Acknowledge> list = new ArrayList<Acknowledge>();
         Acknowledge acknowledge = new Acknowledge();
-        try{
-            NamedParameterJdbcTemplate template = getNamedParameterJdbcTemplate();
+        try {
             SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(eventDetails);
             GeneratedKeyHolder key = new GeneratedKeyHolder();
-            if(eventDetails.getAddressId()!=null && eventDetails.getAddressId()>0){
-                template.update(SQL.UPDATE_EVENT_ADDRESS, namedParameters);
-            }else if(ExpendablesUtil.checkNotNull(eventDetails.getStreetName())){
-                template.update(SQL.SAVE_EVENT_ADDRESS, namedParameters,key);
+            if (eventDetails.getAddressId() != null && eventDetails.getAddressId() > 0) {
+                jdbcTemplate.update(SQL.UPDATE_EVENT_ADDRESS, namedParameters);
+            } else if (ExpendablesUtil.checkNotNull(eventDetails.getStreetName())) {
+                jdbcTemplate.update(SQL.SAVE_EVENT_ADDRESS, namedParameters, key);
                 eventDetails.setAddressId(key.getKey().longValue());
                 acknowledge.setIdNext(eventDetails.getAddressId().longValue());
             }
 
-            template.update(SQL.UPDATE_EVENT, namedParameters);
+            jdbcTemplate.update(SQL.UPDATE_EVENT, namedParameters);
 
             acknowledge.setMessage("User Event details updated. <br/>");
             acknowledge.setId(Long.valueOf(eventDetails.getEventId().toString()));
             list.add(acknowledge);
-            }catch(DataAccessException e){
-                String cause = e.getCause().toString();
-                if(cause !=null){
-                    acknowledge.setMessage(cause.substring(cause.indexOf(":")+1, cause.length())+"<br/>");
-                    list.add(acknowledge);
-                }
-                ExpendablesServiceImpl.LOG.info(e.getMessage());
+        } catch (DataAccessException e) {
+            String cause = e.getCause().toString();
+            if (cause != null) {
+                acknowledge.setMessage(cause.substring(cause.indexOf(":") + 1, cause.length()) + "<br/>");
+                list.add(acknowledge);
             }
-            return list;
+            ExpendablesServiceImpl.LOG.info(e.getMessage());
+        }
+        return list;
     }
 
     public List<EventDTO> getAllUserEvents(String userName) {
-        List<EventDTO> list=new ArrayList<EventDTO>();
-        JdbcTemplate template = new JdbcTemplate(ExpendablesUtil.getDataSource());
+        List<EventDTO> list = new ArrayList<EventDTO>();
         RowMapper<EventDTO> mapper = new RowMapper<EventDTO>() {
 
             public EventDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -134,19 +138,18 @@ public class ExpendablesServiceImpl implements ExpendablesService {
             }
 
         };
-        try{
-                ExpendablesServiceImpl.LOG.info("SQL Events Search "+SQL.GET_EVENT_LIST);
-                list = template.query(SQL.GET_EVENT_LIST,new Object[] {userName}, mapper);
-                ExpendablesServiceImpl.LOG.info("SQL Events size "+ list.size());
-            }catch (DataAccessException e){
-                ExpendablesServiceImpl.LOG.info("Exception cause "+e.getMessage());
-            }
-            return list;
+        try {
+            ExpendablesServiceImpl.LOG.info("SQL Events Search " + SQL.GET_EVENT_LIST);
+            list = jdbcTemplate.query(SQL.GET_EVENT_LIST, new Object[]{userName}, mapper);
+            ExpendablesServiceImpl.LOG.info("SQL Events size " + list.size());
+        } catch (DataAccessException e) {
+            ExpendablesServiceImpl.LOG.info("Exception cause " + e.getMessage());
+        }
+        return list;
     }
 
-    public List<EventDTO> getAllSelectedDateEvents(String userName,String selectedDate){
-        List<EventDTO> list=new ArrayList<EventDTO>();
-        JdbcTemplate template = new JdbcTemplate(ExpendablesUtil.getDataSource());
+    public List<EventDTO> getAllSelectedDateEvents(String userName, String selectedDate) {
+        List<EventDTO> list = new ArrayList<EventDTO>();
         RowMapper<EventDTO> mapper = new RowMapper<EventDTO>() {
 
             public EventDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -163,14 +166,14 @@ public class ExpendablesServiceImpl implements ExpendablesService {
             }
 
         };
-        try{
-                ExpendablesServiceImpl.LOG.info("SQL Events Search "+SQL.GET_EVENT_LIST);
-                list = template.query(SQL.GET_ALL_EVENTS_ON_SELECTED_DATE,new Object[] {selectedDate,userName}, mapper);
-                ExpendablesServiceImpl.LOG.info("SQL Events size "+ list.size());
-            }catch (DataAccessException e){
-                ExpendablesServiceImpl.LOG.info("Exception cause "+e.getMessage());
-            }
-            return list;
+        try {
+            ExpendablesServiceImpl.LOG.info("SQL Events Search " + SQL.GET_EVENT_LIST);
+            list = jdbcTemplate.query(SQL.GET_ALL_EVENTS_ON_SELECTED_DATE, new Object[]{selectedDate, userName}, mapper);
+            ExpendablesServiceImpl.LOG.info("SQL Events size " + list.size());
+        } catch (DataAccessException e) {
+            ExpendablesServiceImpl.LOG.info("Exception cause " + e.getMessage());
+        }
+        return list;
     }
 
     /* (non-Javadoc)
@@ -179,9 +182,8 @@ public class ExpendablesServiceImpl implements ExpendablesService {
     @Override
     public PrincipalDTO getProfilePhoto(String userName) {
         PrincipalDTO photoDTO = new PrincipalDTO();
-        ExpendablesServiceImpl.LOG.info("getProfilePhoto service layer call -------------------"+userName);
+        ExpendablesServiceImpl.LOG.info("getProfilePhoto service layer call -------------------" + userName);
         try {
-            JdbcTemplate template = new JdbcTemplate(ExpendablesUtil.getDataSource());
             RowMapper<PrincipalDTO> mapper = new RowMapper<PrincipalDTO>() {
 
                 public PrincipalDTO mapRow(ResultSet rs, int rowNum)
@@ -194,10 +196,10 @@ public class ExpendablesServiceImpl implements ExpendablesService {
                 }
 
             };
-            photoDTO = template.queryForObject(
-                    "select profilePhoto from users where username=?",  new Object[] { userName }, mapper);
+            photoDTO = jdbcTemplate.queryForObject(
+                    "select profilePhoto from users where username=?", new Object[]{userName}, mapper);
         } catch (Exception e) {
-            ExpendablesServiceImpl.LOG.info("TEST FOR IMAGE DISPLAY  LIST SIZE "+ e.getMessage());
+            ExpendablesServiceImpl.LOG.info("TEST FOR IMAGE DISPLAY  LIST SIZE " + e.getMessage());
         }
         return photoDTO;
     }
@@ -206,21 +208,20 @@ public class ExpendablesServiceImpl implements ExpendablesService {
     public List<Acknowledge> uploadProfilePhoto(PrincipalDTO user) {
         List<Acknowledge> list = new ArrayList<Acknowledge>();
         Acknowledge acknowledge = new Acknowledge();
-        try{
-            NamedParameterJdbcTemplate template = getNamedParameterJdbcTemplate();
+        try {
             SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(user);
-                template.update(SQL.UPDATE_USER_PHOTO, namedParameters);
-                acknowledge.setName("success");
-                acknowledge.setMessage("User profile photo updated. <br/>");
+            jdbcTemplate.update(SQL.UPDATE_USER_PHOTO, namedParameters);
+            acknowledge.setName("success");
+            acknowledge.setMessage("User profile photo updated. <br/>");
+            list.add(acknowledge);
+        } catch (DataAccessException e) {
+            if (e.getCause().toString() != null) {
+                String message = e.getCause().toString();
+                message = message.substring(message.indexOf(":") + 1, message.length()) + "<br/>";
+                acknowledge.setMessage(message);
                 list.add(acknowledge);
-            }catch (DataAccessException e) {
-                if(e.getCause().toString() !=null){
-                    String message= e.getCause().toString();
-                    message = message.substring(message.indexOf(":")+1, message.length())+"<br/>";
-                    acknowledge.setMessage(message);
-                    list.add(acknowledge);
-                }
             }
+        }
         return list;
     }
 
@@ -228,22 +229,21 @@ public class ExpendablesServiceImpl implements ExpendablesService {
     public List<Acknowledge> saveContentDetails(ContentDTO content) {
         List<Acknowledge> list = new ArrayList<Acknowledge>();
         Acknowledge acknowledge = new Acknowledge();
-        try{
-            NamedParameterJdbcTemplate template = getNamedParameterJdbcTemplate();
+        try {
             SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(content);
             GeneratedKeyHolder key = new GeneratedKeyHolder();
 
-            template.update(SQL.CREATE_CONTENT, namedParameters,key);
+            jdbcTemplate.update(SQL.CREATE_CONTENT, namedParameters, key);
             content.setContentId(key.getKey().longValue());
 
             acknowledge.setMessage("Content has been created. <br/>");
             acknowledge.setId(content.getContentId());
 
             //list.add(acknowledge);
-        }catch(DataAccessException e){
+        } catch (DataAccessException e) {
             String cause = e.getCause().toString();
-            if(cause !=null){
-                acknowledge.setMessage(cause.substring(cause.indexOf(":")+1, cause.length())+"<br/>");
+            if (cause != null) {
+                acknowledge.setMessage(cause.substring(cause.indexOf(":") + 1, cause.length()) + "<br/>");
                 list.add(acknowledge);
             }
             ExpendablesServiceImpl.LOG.info(e.getMessage());
@@ -255,22 +255,22 @@ public class ExpendablesServiceImpl implements ExpendablesService {
     public List<Acknowledge> updateContentDetails(ContentDTO content) {
         List<Acknowledge> list = new ArrayList<Acknowledge>();
         Acknowledge acknowledge = new Acknowledge();
-        try{
-            NamedParameterJdbcTemplate template = getNamedParameterJdbcTemplate();
+        try {
+
             SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(content);
             GeneratedKeyHolder key = new GeneratedKeyHolder();
 
-            template.update(SQL.DELETE_CONTENT_BY_ID, namedParameters,key);
+            jdbcTemplate.update(SQL.DELETE_CONTENT_BY_ID, namedParameters, key);
             //content.setContent_id(key.getKey().longValue());
 
             acknowledge.setMessage("Content has been created. <br/>");
             acknowledge.setName(content.getContentName());
 
             list.add(acknowledge);
-        }catch(DataAccessException e){
+        } catch (DataAccessException e) {
             String cause = e.getCause().toString();
-            if(cause !=null){
-                acknowledge.setMessage(cause.substring(cause.indexOf(":")+1, cause.length())+"<br/>");
+            if (cause != null) {
+                acknowledge.setMessage(cause.substring(cause.indexOf(":") + 1, cause.length()) + "<br/>");
                 list.add(acknowledge);
             }
             ExpendablesServiceImpl.LOG.info(e.getMessage());
